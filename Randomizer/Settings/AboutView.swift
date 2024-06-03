00137ca9cace7c46a04d79e0ff06675f3b99c3b9
WimPum <n1v0x.5127e@gmail.com>
2024-06-03 23:45:42 +0900

    About screen

---
+++ Randomizer/Settings/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  Randomizer
+//
+//  Created by 虎澤謙 on 2024/06/03.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
