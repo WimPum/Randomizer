dde0f84f04c3f26642225a1b4c699e7827e4c211
WimPum <n1v0x.5127e@gmail.com>
2024-07-23 00:37:47 +0900

    Add helpview.swift

---
+++ Randomizer/Settings/HelpView.swift
@@ -0,0 +1,18 @@
+//
+//  HelpView.swift
+//  Randomizer
+//
+//  Created by 虎澤謙 on 2024/07/07.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelpView()
+}
