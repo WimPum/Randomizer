eaa4ec0daac2e820fbad8970a5ee4629725df18d
WimPum <n1v0x.5127e@gmail.com>
2024-07-27 18:28:13 +0900

    Saving the HISTORY!!!

---
+++ Randomizer/Data/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  Randomizer
+//
+//  Created by 虎澤謙 on 2024/07/26.
+//
+
+import Foundation
---
+++ WhatData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WhatData+CoreDataClass.swift
+//  Randomizer
+//
+//  Created by 虎澤謙 on 2024/07/26.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WhatData)
+public class WhatData: NSManagedObject {
+
+}
