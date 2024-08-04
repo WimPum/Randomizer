158931e1476c755ecf51e5bf80b72f147a917562
WimPum <n1v0x.5127e@gmail.com>
2024-08-04 16:56:15 +0900

    add AnimGradient.swift

---
+++ Randomizer/View/CustomElement/AnimGradient.swift
@@ -0,0 +1,18 @@
+//
+//  AnimGradient.swift
+//  Randomizer
+//
+//  Created by 虎澤謙 on 2024/08/04.
+//
+
+import SwiftUI
+
+struct AnimGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimGradient()
+}
